446292c298f05f5c59d190340674ec92677ed9e4
takshingli810 <takshingli810@gmail.com>
2016-02-22 19:05:31 -0800

    added a edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    @user.save
+    redirect_to user_path(@user)
+  end
+
 end

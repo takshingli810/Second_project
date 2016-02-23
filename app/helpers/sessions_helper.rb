02065000071579ba5fa51e316cf43af94c718801
takshingli810 <takshingli810@gmail.com>
2016-02-23 09:33:06 -0800

    added session.helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+
+  def login(user)
+    session[:user_id] = user.user_id
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    if current_user == nil
+      redirect_to "sign_in"
+    end
+  end
+
+  def logout
+    @current_user = session[:user[id]] = nil
+  end
+
 end

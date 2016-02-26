7292ca704281c1c094a7d0db7788560840cd592a
takshingli810 <takshingli810@gmail.com>
2016-02-25 21:38:12 -0800

    fixed issue id

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,10 +2,11 @@ class PicturesController < ApplicationController
   
   def addPictureToGroup
     @group = Group.find(params[:id])
+    
     @picture = @group.pictures.new(picture_params)
     if @picture.save 
-      redirect_to user_group_path(@group)
-    end
+      redirect_to "/users/#{current_user.id}/groups/#{@group.id}"
+      end
     
   end
 
@@ -13,7 +14,7 @@ class PicturesController < ApplicationController
   private
 
   def picture_params
-    params.require(:picture).permit(:url)
+    params.require(:picture).permit(:url, :id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,3 +28,26 @@ Rails.application.routes.draw do
 
 
 end
+#            Prefix Verb   URI Pattern                               Controller#Action
+#              root GET    /                                         users#index
+#       user_groups GET    /users/:user_id/groups(.:format)          groups#index
+#                   POST   /users/:user_id/groups(.:format)          groups#create
+#    new_user_group GET    /users/:user_id/groups/new(.:format)      groups#new
+#   edit_user_group GET    /users/:user_id/groups/:id/edit(.:format) groups#edit
+#        user_group GET    /users/:user_id/groups/:id(.:format)      groups#show
+#                   PATCH  /users/:user_id/groups/:id(.:format)      groups#update
+#                   PUT    /users/:user_id/groups/:id(.:format)      groups#update
+#                   DELETE /users/:user_id/groups/:id(.:format)      groups#destroy
+#             users GET    /users(.:format)                          users#index
+#                   POST   /users(.:format)                          users#create
+#          new_user GET    /users/new(.:format)                      users#new
+#         edit_user GET    /users/:id/edit(.:format)                 users#edit
+#              user GET    /users/:id(.:format)                      users#show
+#                   PATCH  /users/:id(.:format)                      users#update
+#                   PUT    /users/:id(.:format)                      users#update
+#                   DELETE /users/:id(.:format)                      users#destroy
+#  membership_index GET    /users/:user_id/groups(.:format)          memberships#index
+# membership_create POST   /groups/:group_id/users(.:format)         memberships#create
+#           sign_in GET    /sign_in(.:format)                        sessions#new
+#          sessions POST   /sessions(.:format)                       sessions#create
+#                   DELETE /sessions(.:format)                       sessions#destroy

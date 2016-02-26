0db3237ee9c3cb51046d2d90a6810dd4823aa77e
takshingli810 <takshingli810@gmail.com>
2016-02-25 16:09:50 -0800

    member pushed

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,17 @@
+class MembershipsController < ApplicationController
+  
+  before_action :logged_in?
+  def index 
+    @user = User.find(params[:user_id])
+    @group = @user.groups
+    render :index
+  end
+
+  def create 
+    @user = current_user
+    @group = Group.find(params[:group_id])
+    @user.groups.push(@group)
+    redirect user_group_path(@user)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
    resources :groups
   end
 
+  get "/users/:user_id/groups", to: "memberships#index", as:"membership_index"
+  post "/groups/:group_id/users", to: "memberships#create", as: "membership_create"
+
   # sign in route for user session
   get "/sign_in", to: "sessions#new"
 
